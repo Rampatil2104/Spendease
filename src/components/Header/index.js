48bd24b28b09e8075ed37b9a153bff3a76dcba66
Ram Patil <ram.patil4021@gmail.com>
2024-10-07 10:46:53 -0700

    Initial Push
    spendease initial push

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./styles.css";
+
+function Header() {
+  return (
+    <div className='navbar'>
+    <p style = {{color:"var(--white", fontWeight:600,fontSize:"1.2rem"}}>Spendease</p>
+    </div>
+  )
+}
+
+export default Header;
